c66184777666abb73191b544b5e40c90b4ffc9e4
fu <fu@13704.com>
2021-01-05 02:38:07 +0800

    初始提交

---
+++ src/main/java/com/fu/domain/Person.java
@@ -0,0 +1,13 @@
+package com.fu.domain;
+
+/**
+ * ClassName:Person
+ * Package:com.fu.domain
+ * description:
+ *
+ * @Author: xiaofu
+ * @Datea: 2021/1/5 - 2:17
+ */
+public class Person {
+    private String id;
+}
